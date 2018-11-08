07e5dd14cd5d090ab14ee519e09259f91110302d
Fang Duan <fang.duan.hu@gmail.com>
2018-11-08 23:52:26 +0100

    add get_folds; modify 'expm1'

--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import GroupKFold
+
+def get_folds(df = None, n_splits = 5):
+    unique_vis = np.array(sorted(df['fullVisitorId'].unique()))
+    folds = GroupKFold(n_splits = n_splits)
+    fold_ids = []
+    ids = np.arange(df.shape[0])
+    for train_vis, test_vis in folds.split(X = unique_vis, y = unique_vis, groups = unique_vis):
+        fold_ids.append(
+            [
+                ids[df['fullVisitorId'].isin(unique_vis[train_vis])],
+                ids[df['fullVisitorId'].isin(unique_vis[test_vis])]
+            ]
+        )
+    return fold_ids
 
 def create_submission(pred,test):
 
     pred = pd.DataFrame(pred)
-    test["PredictedLogRevenue"] = np.expm1(pred)
+    test["PredictedLogRevenue"] = pred
     submission = test.groupby("fullVisitorId").agg({"PredictedLogRevenue" : "sum"}).reset_index()
     submission["PredictedLogRevenue"] = np.log1p(submission["PredictedLogRevenue"])
     submission["PredictedLogRevenue"] =  submission["PredictedLogRevenue"].apply(lambda x : 0.0 if x < 0 else x)
